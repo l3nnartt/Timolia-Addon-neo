f4605317dbe9b18920db7020d7096f975d61d4bd
Lennart Lösche <loesche.lennart@gmail.com>
2023-02-08 13:55:24 +0100

    init packets lol

--- core/src/main/java/com/github/l3nnartt/timolia/core/TimoliaProtocol.java
+++ core/src/main/java/com/github/l3nnartt/timolia/core/TimoliaProtocol.java
@@ -11,9 +11,9 @@ public class TimoliaProtocol extends AddonProtocol {
   public TimoliaProtocol() {
     super("timolia");
 
-    this.registerPacket(0, new StreakPacket());
-    this.registerPacket(1, new MatchPacket());
-    this.registerPacket(2, new EnemyPacket());
-    this.registerPacket(3, new MapPacket());
+    this.registerPacket(0, new StreakPacket(""));
+    this.registerPacket(1, new MatchPacket("","",""));
+    this.registerPacket(2, new EnemyPacket(""));
+    this.registerPacket(3, new MapPacket(""));
   }
 }
